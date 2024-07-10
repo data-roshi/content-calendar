eea023e610cd89e7949865532a9e09bfa316e492
data-roshi <dataroshi@gmail.com>
2024-07-10 12:16:47 +0200

    find by id pathvariable

--- src/main/java/com/dataroshi/contentcalendar/controller/ContentController.java
+++ src/main/java/com/dataroshi/contentcalendar/controller/ContentController.java
@@ -2,9 +2,14 @@ package com.dataroshi.contentcalendar.controller;
 
 import com.dataroshi.contentcalendar.model.Content;
 import com.dataroshi.contentcalendar.repository.ContentCollectionRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -13,6 +18,7 @@ import java.util.List;
 public class ContentController {
 
 
+    private static final Logger LOG = LoggerFactory.getLogger(ContentController.class);
 
     private final ContentCollectionRepository contentCollectionRepository;
 
@@ -23,5 +29,12 @@ public class ContentController {
     @GetMapping("")
     public List<Content> findAll() {
         return contentCollectionRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Content findById(@PathVariable Integer id) {
+        return contentCollectionRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        String.format("Content with id %s not found", id)));
     }
 }
--- src/main/java/com/dataroshi/contentcalendar/repository/ContentCollectionRepository.java
+++ src/main/java/com/dataroshi/contentcalendar/repository/ContentCollectionRepository.java
@@ -1,8 +1,12 @@
 package com.dataroshi.contentcalendar.repository;
 
 import com.dataroshi.contentcalendar.model.Content;
+import com.dataroshi.contentcalendar.model.Status;
+import com.dataroshi.contentcalendar.model.Type;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -11,16 +15,30 @@ import java.util.Optional;
 @Repository
 public class ContentCollectionRepository {
 
-    private final List<Content> content = new ArrayList<>();
+    private final List<Content> contents = new ArrayList<>();
 
     public List<Content> findAll() {
-        return content;
+        return contents;
     }
 
     public Optional<Content> findById(Integer id) {
-       return content.stream()
-               .filter(Objects::nonNull)
-               .filter(c -> c.id().equals(id))
-               .findAny();
+        return contents.stream()
+                .filter(Objects::nonNull)
+                .filter(c -> c.id().equals(id))
+                .findAny();
+    }
+
+    @PostConstruct
+    private void onInit() {
+        var content = new Content(1,
+                "Blog Post 1",
+                "Blog post 1",
+                Status.IDEA,
+                Type.ARTICLE,
+                LocalDateTime.now(),
+                null,
+                "");
+
+        contents.add(content);
     }
 }
