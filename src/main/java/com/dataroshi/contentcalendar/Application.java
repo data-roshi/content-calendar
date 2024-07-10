48aa2bbd11f1bddfdce1526c6182da3d7107147f
data-roshi <dataroshi@gmail.com>
2024-07-10 10:11:16 +0200

    application context beans logging

--- src/main/java/com/dataroshi/contentcalendar/Application.java
+++ src/main/java/com/dataroshi/contentcalendar/Application.java
@@ -1,13 +1,20 @@
 package com.dataroshi.contentcalendar;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+	private static final Logger LOG = LoggerFactory.getLogger(Application.class);
 
+    public static void main(String[] args) {
+        var context = SpringApplication.run(Application.class, args);
+
+		Arrays.stream(context.getBeanDefinitionNames()).forEach(LOG::info);
+    }
 }
