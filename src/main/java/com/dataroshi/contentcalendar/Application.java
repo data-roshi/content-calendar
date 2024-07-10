3aa3909f999c69f94918c2d284854f363996c116
data-roshi <dataroshi@gmail.com>
2024-07-10 10:33:38 +0200

    webconfig bean

--- src/main/java/com/dataroshi/contentcalendar/Application.java
+++ src/main/java/com/dataroshi/contentcalendar/Application.java
@@ -5,16 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-
 @SpringBootApplication
 public class Application {
 
-	private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
-        var context = SpringApplication.run(Application.class, args);
-
-		Arrays.stream(context.getBeanDefinitionNames()).forEach(LOG::info);
+        SpringApplication.run(Application.class, args);
     }
 }
